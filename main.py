cfbbaf77272d000ac9b0959cad0f44cdf08d9e06
FedorDr <>
2022-02-19 13:39:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from turtle import * 
+shape('turtle')
+fd(50)
+right(90)
+fd(50)
+right(90)
+fd(50)
+right(90)
+fd(50)
+right(90)
+from turtle import * 
+shape('turtle')
+
+color('red','green')
+fd(50)
+right(90)
+fd(50)
+right(90)
+fd(50)
+right(90)
+fd(50)
+right(90)
+
+begin_fill()
+circle(50)
+fd(50)
+end_fill()
+
+circle(50)
