6b74318657e04a23a8f591dc549ce35726ea2277
Fedor Dr <100030842+fedor705804@users.noreply.github.com>
2022-02-20 10:21:37 +0000

    орден

--- main.py
+++ main.py
@@ -2,32 +2,30 @@ from turtle import *
 
 size = 150
 
-shape('turtle')
-fd(size)
-right(90)
-fd(size)
-right(90)
-fd(size)
-right(90)
-fd(size)
-right(90)
-from turtle import *
+seze = 75
 
-shape('turtle')
+suze = 50
 
-color('red', 'green')
-fd(size)
-right(90)
-fd(size)
-right(90)
-fd(size)
-right(90)
-fd(size)
-right(90)
+color('yellow','blue')
 
 begin_fill()
-circle(size)
+shape('turtle')
 fd(size)
+right(90)
+fd(seze)
+right(90)
+fd(size)
+right(90)
+fd(seze)
+right(90)
+fd(seze)
 end_fill()
 
-circle(size)
+color('yellow')
+begin_fill()
+right(90)
+fd(seze)
+right(90)
+circle(50)
+shape('turtle')
+end_fill()
